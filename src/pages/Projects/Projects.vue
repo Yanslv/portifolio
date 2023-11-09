f613c8ca79e8c7fc86560755e6fdffdb419955c0
Yan Amorim <yan.amorim.silva@gmail.com>
2023-11-09 18:04:16 -0400

    alterações no card

--- src/pages/Projects/Projects.vue
+++ src/pages/Projects/Projects.vue
@@ -1,20 +1,55 @@
 <template>
-        <div class="w-96 h-44 bg-emerald-500 ml-96 mt-12 mb-4 rounded-md bg-opacity-50 flex justify-between flex-col">
-            <div class="px-4 pt-2 flex justify-between">
-                <div class="flex flex-col">
-                    <span class="text-lg">
-                        My Todo
-                    </span>
-                    <span class="mt-2">
-                    Nesse aplicativo eu faço uma lista de tarefas.
-                    </span>
+        <div class="pl-96 mt-12 pr-80">
+            <div class="border-b-emerald-400 border-b-2 border-solid">
+                <span class="text-2xl">
+                    Meus aplicativos
+                </span>
+            </div>
+        </div>
+        <div class="grid grid-cols-2">
+            <div class="w-96 h-44 bg-emerald-900 ml-96 mt-8 mb-4 rounded-md bg-opacity-40 flex justify-between flex-col">
+                <div class="px-4 pt-2 flex justify-between">
+                    <div class="flex flex-col">
+                        <span class="text-lg">
+                            My Todo
+                        </span>
+                    </div>
+                    <div class=" flex justify-end text-tahiti-400">
+                        ir
+                    </div>
                 </div>
-                <div class=" flex justify-end text-tahiti-400">
-                    ir
+                <div class="flex justify-between h-36">
+                    <div class="items-end flex mx-4 mb-2">
+                        <img src="../../assets/img/react-native.jpg" class="h-32 rounded-md w-44 justify-end flex items-end ">
+                    </div>
+                    <div class="mr-4">
+                        <span>
+                        Nesse aplicativo eu faço uma lista de tarefas.
+                        </span>
+                    </div>
                 </div>
             </div>
-            <div class="flex justify-end">
-                <img src="../../assets/img/react-native.jpg" class="h-16 rounded-md w-28 justify-end flex items-end ">
+            <div class="w-96 h-44 bg-emerald-900 ml-96 mt-8 mb-4 rounded-md bg-opacity-40 flex justify-between flex-col">
+                <div class="px-4 pt-2 flex justify-between">
+                    <div class="flex flex-col">
+                        <span class="text-lg">
+                            My Todo
+                        </span>
+                    </div>
+                    <div class=" flex justify-end text-tahiti-400">
+                        ir
+                    </div>
+                </div>
+                <div class="flex justify-between h-36">
+                    <div class="items-end flex mx-4 mb-2">
+                        <img src="../../assets/img/react-native.jpg" class="h-32 rounded-md w-44 justify-end flex items-end ">
+                    </div>
+                    <div class="mr-4">
+                        <span>
+                        Nesse aplicativo eu faço uma lista de tarefas.
+                        </span>
+                    </div>
+                </div>
             </div>
         </div>
 </template>
